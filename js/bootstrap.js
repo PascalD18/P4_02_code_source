1a02e88b640fd0173ed253f9ff0a56a175248821
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-02-26 09:22:17 +0100

    correction bootstrap

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -6,7 +6,7 @@
 if ("undefined" == typeof jQuery) throw new Error("Bootstrap JavaScript requires jQuery"); + function(a) {
     "use strict";
     var b = a.fn.jquery.split(" ")[0].split(".");
-    if (b[0] < 2 && b[1] < 9 || 1 == b[0] && 9 == b[1] && b[2] < 1) throw new Error("Bootstrap's JavaScript requires jQuery version 1.9.1 or higher")
+    if (b[0] < 2 && b[1] < 9 || 1 == b[0] && 9 == b[1] && b[2] < 1) throw new Error("Bootstrap JavaScript requires jQuery version 1.9.1 or higher")
 }(jQuery), + function(a) {
     "use strict";
 
