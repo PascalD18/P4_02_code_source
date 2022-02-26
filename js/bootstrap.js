288fcd3665032fb550fb111383f54a4eda1932de
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-02-26 09:26:28 +0100

    correction bootstrap

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,10 +3,10 @@
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under the MIT license
  */
-if ("undefined" == typeof jQuery) throw new Error("Bootstrap JavaScript requires jQuery"); + function(a) {
+if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requires jQuery version 1.9.1 or higher"); + function(a) {
     "use strict";
     var b = a.fn.jquery.split(" ")[0].split(".");
-    if (b[0] < 2 && b[1] < 9 || 1 == b[0] && 9 == b[1] && b[2] < 1) throw new Error("Bootstrap JavaScript requires jQuery version 1.9.1 or higher")
+    if (b[0] < 2 && b[1] < 9 || 1 == b[0] && 9 == b[1] && b[2] < 1) throw new Error("Bootstrap's JavaScript requires jQuery version 1.9.1 or higher")
 }(jQuery), + function(a) {
     "use strict";
 
